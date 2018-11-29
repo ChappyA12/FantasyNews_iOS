e811d1408d7fc157d06efef80c12c3fbbde03ba2
ChappyA12 <chappy_asel@hotmail.com>
2018-11-28 16:12:08 -0800

    Added app

---
+++ Fantasy Sports News/Fantasy Sports News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fantasy Sports News
+//
+//  Created by Chappy Asel on 11/28/18.
+//  Copyright © 2018 CA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fantasy Sports News/Fantasy Sports News/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Fantasy Sports News
+//
+//  Created by Chappy Asel on 11/28/18.
+//  Copyright © 2018 CA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
